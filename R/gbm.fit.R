gbm-developers/gbm3

commit f2f8c8f3cf3c494a7af3f86e59c256f72e50b0b0

making test for Surv objects more flexible in gbm.fitt. Google Code issue 29

R/gbm.fit.R
@@ -23,7 +23,7 @@ gbm.fit <- function(x,y,
    cRows <- nrow(x)
    cCols <- ncol(x)
 
-   if(nrow(x) != ifelse(class(y)=="Surv", nrow(y), length(y))) {
+   if(nrow(x) != ifelse("Surv" %in% class(junk), nrow(y), length(y))) {
       stop("The number of rows in x does not equal the length of y.")
    }
 

commit 8f8f11060d63c727ad5ac9ef7a8c8db1977e93d9

Fix bug introduced in commit f2f8c8f3cf3c494a
The object "junk" does not exist

R/gbm.fit.R
@@ -23,7 +23,7 @@ gbm.fit <- function(x,y,
    cRows <- nrow(x)
    cCols <- ncol(x)
 
-   if(nrow(x) != ifelse("Surv" %in% class(junk), nrow(y), length(y))) {
+   if(nrow(x) != ifelse("Surv" %in% class(y), nrow(y), length(y))) {
       stop("The number of rows in x does not equal the length of y.")
    }
 