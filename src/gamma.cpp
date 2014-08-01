9544dc80fb01eb714d335f3576ab763635494de7
RyanMichaluk <rmick@allstate.com>
2014-08-01 16:05:06 -0500

    gamma and tweedie distributions

--- src/gamma.cpp
+++ src/gamma.cpp
@@ -129,7 +129,7 @@ GBMRESULT CGamma::FitBestConstant
     double *adW,
     double *adF,
     double *adZ,
-    unsigned long *aiNodeAssign,
+    const std::vector<unsigned long>& aiNodeAssign,
     unsigned long nTrain,
     VEC_P_NODETERMINAL vecpTermNodes,
     unsigned long cTermNodes,
