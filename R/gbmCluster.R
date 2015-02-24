b5a2f17f2673125e5e08ce47912b94e2abd46e7a
harry <harry@dataclarityconsulting.co.uk>
2015-02-24 16:12:51 +0000

    Telling gbmCluster where to find detectCores

--- R/gbmCluster.R
+++ R/gbmCluster.R
@@ -2,7 +2,7 @@ gbmCluster <- function(n){
     # If number of cores (n) not given, try to work it out from the number
     # that appear to be available and the number of CV folds.
     if (is.null(n)){
-      n <- detectCores()
+      n <- parallel::detectCores()
     }
     if (n == 1){ NULL }
     else { parallel::makeCluster(n) }
