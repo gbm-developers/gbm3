4979f3809dcabbaaa1a48440c9dbbfd0081f5d78
Paul Metcalfe <paul.metcalfe@gmail.com>
2015-03-16 10:34:06 +0000

    Ensure that core detection works on toy machines
    [otherwise everything fails interestingly]

--- R/gbmCluster.R
+++ R/gbmCluster.R
@@ -2,7 +2,7 @@ gbmCluster <- function(n){
     # If number of cores (n) not given, try to work it out from the number
     # that appear to be available and the number of CV folds.
     if (is.null(n)){
-      n <- parallel::detectCores() - 1
+      n <- max(1, parallel::detectCores() - 1)
     }
     if (n == 1){ NULL }
     else { parallel::makeCluster(n) }
