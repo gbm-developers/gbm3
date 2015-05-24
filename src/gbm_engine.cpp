063fd394fb52bb836177ab6253d6b652c7eaa565
Greg Ridgeway <gregridgeway@gmail.com>
2015-05-24 15:26:31 -0400

    cast unsigned longs to int
    Build generated warnings when unsigned long cTrain was compared with
    data.nrow(). Now cTrain explicitly cast to int to suppress the warning.
    Also fixed code indentation throughout.
    Also added additional debugging information when NOISY_DEBUG build flag
    set

--- src/gbm_engine.cpp
+++ src/gbm_engine.cpp
@@ -61,7 +61,7 @@ void CGBM::Initialize
   
   cValid = data.nrow() - cTrain;
 
-  if ((cTrain <= 0) || (data.nrow() < cTrain)) {
+  if ((cTrain <= 0) || (data.nrow() < int(cTrain))) {
     throw GBM::invalid_argument("your training instances don't make sense");
   }
   
@@ -98,208 +98,222 @@ void CGBM::Initialize
 
 void CGBM::iterate
 (
-    double *adF,
-    double &dTrainError,
-    double &dValidError,
-    double &dOOBagImprove,
-    int &cNodes,
-    int cNumClasses,
-    int cClassIdx
+  double *adF,
+  double &dTrainError,
+  double &dValidError,
+  double &dOOBagImprove,
+  int &cNodes,
+  int cNumClasses,
+  int cClassIdx
 )
 {
-    unsigned long i = 0;
-    unsigned long cBagged = 0;
-    int cIdxOff = cClassIdx * (cTrain + cValid);
+  unsigned long i = 0;
+  unsigned long cBagged = 0;
+  int cIdxOff = cClassIdx * (cTrain + cValid);
 
-    if(!fInitialized)
-      {
-	throw GBM::failure();
-      }
+  if(!fInitialized)
+  {
+    throw GBM::failure();
+  }
 
-    dTrainError = 0.0;
-    dValidError = 0.0;
-    dOOBagImprove = 0.0;
+  dTrainError = 0.0;
+  dValidError = 0.0;
+  dOOBagImprove = 0.0;
 
-    vecpTermNodes.assign(2*cDepth+1,NULL);
+  vecpTermNodes.assign(2*cDepth+1,NULL);
 
-    // randomly assign observations to the Bag
-
-    if (cClassIdx == 0)
+  // randomly assign observations to the Bag
+  if (cClassIdx == 0)
+  {
+    if (!IsPairwise())
     {
-        if (!IsPairwise())
+      // regular instance based training
+      for(i=0; i<cTrain; i++)
+      {
+        if(unif_rand()*(cTrain-i) < cTotalInBag-cBagged)
         {
-            // regular instance based training
-            for(i=0; i<cTrain; i++) /* && (cBagged < cTotalInBag); i++) */
-            {
-                if(unif_rand()*(cTrain-i) < cTotalInBag-cBagged)
-                {
-                    afInBag[i] = true;
-                    cBagged++;
-                }
-                else
-                {
-                    afInBag[i] = false;
-                }
-/*                if (cBagged >= cTotalInBag){
-                    break;
-                } */
-            }
-	    std::fill(afInBag.begin() + i, afInBag.end(), false);
+          afInBag[i] = true;
+          cBagged++;
         }
         else
         {
-            // for pairwise training, sampling is per group
-            // therefore, we will not have exactly cTotalInBag instances
-
-            double dLastGroup = -1;
-            bool chosen = false;
-            unsigned int cBaggedGroups = 0;
-            unsigned int cSeenGroups   = 0;
-            unsigned int cTotalGroupsInBag = (unsigned long)(dBagFraction * cGroups);
-            if (cTotalGroupsInBag <= 0)
-            {
-                cTotalGroupsInBag = 1;
-            }
-            for(i=0; i<cTrain; i++)
-            {
-              const double dGroup = pData->misc_ptr(true)[i];
-              if (dGroup != dLastGroup)
-                {
-                  if (cBaggedGroups >= cTotalGroupsInBag)
-                    {
-                      break;
-                    }
-                  
-                  // Group changed, make a new decision
-                  chosen = (unif_rand()*(cGroups - cSeenGroups) < cTotalGroupsInBag - cBaggedGroups);
-                  if (chosen)
-                    {
-                      cBaggedGroups++;
-                    }
-                  dLastGroup = dGroup;
-                  cSeenGroups++;
-                }
-              if (chosen)
-                {
-                  afInBag[i] = true;
-                  cBagged++;
-                }
-              else
-                {
-                  afInBag[i] = false;
-                }
-            }
-            // the remainder is not in the bag
-	    std::fill(afInBag.begin() + i, afInBag.end(), false);
+          afInBag[i] = false;
         }
+      }
+	    std::fill(afInBag.begin() + i, afInBag.end(), false);
     }
-
-#ifdef NOISY_DEBUG
-    Rprintf("Compute working response\n");
-#endif
-
-    pDist->ComputeWorkingResponse(pData->y_ptr(),
-				  pData->misc_ptr(false),
-				  pData->offset_ptr(false),
-				  adF,
-				  &adZ[0],
-				  pData->weight_ptr(),
-				  afInBag,
-				  cTrain,
-				  cIdxOff);
-
-#ifdef NOISY_DEBUG
-    Rprintf("Reset tree\n");
-#endif
-    ptreeTemp->Reset();
-#ifdef NOISY_DEBUG
-    Rprintf("grow tree\n");
-#endif
-
-    ptreeTemp->grow(&(adZ[cIdxOff]), *pData, pData->weight_ptr() + cIdxOff,
-		    &(adFadj[cIdxOff]), cTrain, cFeatures, cTotalInBag, 
-		    dLambda, cDepth,
-		    cMinObsInNode, afInBag, aiNodeAssign, &aNodeSearch[0],
-		    vecpTermNodes);
-    
-    
-#ifdef NOISY_DEBUG
-    Rprintf("get node count\n");
-#endif
-    ptreeTemp->GetNodeCount(cNodes);
-
-    // Now I have adF, adZ, and vecpTermNodes (new node assignments)
-    // Fit the best constant within each terminal node
-#ifdef NOISY_DEBUG
-    Rprintf("fit best constant\n");
-#endif
-
-    pDist->FitBestConstant(pData->y_ptr(),
-			   pData->misc_ptr(false),
-			   pData->offset_ptr(false),
-			   pData->weight_ptr(),
-			   &adF[0],
-			   &adZ[0],
-			   aiNodeAssign,
-			   cTrain,
-			   vecpTermNodes,
-			   (2*cNodes+1)/3, // number of terminal nodes
-			   cMinObsInNode,
-			   afInBag,
-			   &adFadj[0],
-			   cIdxOff);
-    
-    
-    // update training predictions
-    // fill in missing nodes where N < cMinObsInNode
-    ptreeTemp->Adjust(aiNodeAssign,&(adFadj[cIdxOff]),cTrain,
-		      vecpTermNodes,cMinObsInNode);
-    ptreeTemp->SetShrinkage(dLambda);
-
-    if (cClassIdx == (cNumClasses - 1))
+    else
     {
-      dOOBagImprove = pDist->BagImprovement(pData->y_ptr(),
-					    pData->misc_ptr(false),
-					    pData->offset_ptr(false),
-					    pData->weight_ptr(),
-					    &adF[0],
-					    &adFadj[0],
-					    afInBag,
-					    dLambda,
-					    cTrain);
+      // for pairwise training, sampling is per group
+      // therefore, we will not have exactly cTotalInBag instances
+      double dLastGroup = -1;
+      bool fChosen = false;
+      unsigned int cBaggedGroups = 0;
+      unsigned int cSeenGroups   = 0;
+      unsigned int cTotalGroupsInBag = (unsigned long)(dBagFraction * cGroups);
+      if (cTotalGroupsInBag <= 0)
+      {
+        cTotalGroupsInBag = 1;
+      }
+      for(i=0; i<cTrain; i++)
+      {
+        const double dGroup = pData->misc_ptr(true)[i];
+        if(dGroup != dLastGroup)
+        {
+          if (cBaggedGroups >= cTotalGroupsInBag)
+          {
+            break;
+          }
+                  
+          // Group changed, make a new decision
+          fChosen = (unif_rand()*(cGroups - cSeenGroups) < 
+                   cTotalGroupsInBag - cBaggedGroups);
+          if(fChosen)
+          {
+            cBaggedGroups++;
+          }
+          dLastGroup = dGroup;
+          cSeenGroups++;
+        }
+        if(fChosen)
+        {
+          afInBag[i] = true;
+          cBagged++;
+        }
+        else
+        {
+          afInBag[i] = false;
+        }
+      }
+      // the remainder is not in the bag
+	    std::fill(afInBag.begin() + i, afInBag.end(), false);
     }
+  }
+
+#ifdef NOISY_DEBUG
+  Rprintf("Compute working response\n");
+#endif
+
+  pDist->ComputeWorkingResponse(pData->y_ptr(),
+                      				  pData->misc_ptr(false),
+                      				  pData->offset_ptr(false),
+                      				  adF,
+                      				  &adZ[0],
+                      				  pData->weight_ptr(),
+                      				  afInBag,
+                      				  cTrain,
+                      				  cIdxOff);
+
+#ifdef NOISY_DEBUG
+  Rprintf("Reset tree\n");
+#endif
+  ptreeTemp->Reset();
+#ifdef NOISY_DEBUG
+  Rprintf("grow tree\n");
+#endif
+
+  ptreeTemp->grow(&(adZ[cIdxOff]), 
+                  *pData, 
+                  pData->weight_ptr() + cIdxOff,
+	                &(adFadj[cIdxOff]), 
+                  cTrain, 
+                  cFeatures, 
+                  cTotalInBag, 
+	                dLambda, 
+                  cDepth,
+	                cMinObsInNode, 
+                  afInBag, 
+                  aiNodeAssign, 
+                  &aNodeSearch[0],
+	                vecpTermNodes);
+
+#ifdef NOISY_DEBUG
+  ptreeTemp->Print();
+#endif
+
+  ptreeTemp->GetNodeCount(cNodes);
+#ifdef NOISY_DEBUG
+  Rprintf("get node count=%d\n",cNodes);
+#endif
+
+  // Now I have adF, adZ, and vecpTermNodes (new node assignments)
+  // Fit the best constant within each terminal node
+#ifdef NOISY_DEBUG
+  Rprintf("fit best constant\n");
+#endif
+
+  pDist->FitBestConstant(pData->y_ptr(),
+                			   pData->misc_ptr(false),
+                			   pData->offset_ptr(false),
+                			   pData->weight_ptr(),
+                			   &adF[0],
+                			   &adZ[0],
+                			   aiNodeAssign,
+                			   cTrain,
+                			   vecpTermNodes,
+                			   (2*cNodes+1)/3, // number of terminal nodes
+                			   cMinObsInNode,
+                			   afInBag,
+                			   &adFadj[0],
+                			   cIdxOff);
+
+  // update training predictions
+  // fill in missing nodes where N < cMinObsInNode
+  ptreeTemp->Adjust(aiNodeAssign,
+                    &(adFadj[cIdxOff]),
+                    cTrain,
+                    vecpTermNodes,
+                    cMinObsInNode);
+  ptreeTemp->SetShrinkage(dLambda);
+#ifdef NOISY_DEBUG
+  ptreeTemp->Print();
+#endif
+
+  if (cClassIdx == (cNumClasses - 1))
+  {
+    dOOBagImprove = pDist->BagImprovement(pData->y_ptr(),
+                            					    pData->misc_ptr(false),
+                            					    pData->offset_ptr(false),
+                            					    pData->weight_ptr(),
+                            					    &adF[0],
+                            					    &adFadj[0],
+                            					    afInBag,
+                            					    dLambda,
+                            					    cTrain);
+  }
     
-    // update the training predictions
-    for(i=0; i < cTrain; i++)
-      {
-        int iIdx = i + cIdxOff;
-        adF[iIdx] += dLambda * adFadj[iIdx];
-      }
+  // update the training predictions
+  int iIdx = 0;
+  for(i=0; i < cTrain; i++)
+  {
+    iIdx = i + cIdxOff;
+    adF[iIdx] += dLambda * adFadj[iIdx];
+  }
 
-    dTrainError = pDist->Deviance(pData->y_ptr(),
-                                  pData->misc_ptr(false),
-                                  pData->offset_ptr(false),
-                                  pData->weight_ptr(),
-                                  adF,
-                                  cTrain,
-                                  cIdxOff);
+  dTrainError = pDist->Deviance(pData->y_ptr(),
+                                pData->misc_ptr(false),
+                                pData->offset_ptr(false),
+                                pData->weight_ptr(),
+                                adF,
+                                cTrain,
+                                cIdxOff);
 
-    // update the validation predictions
-    ptreeTemp->PredictValid(*pData,cValid,&(adFadj[cIdxOff]));
+  // update the validation predictions
+  ptreeTemp->PredictValid(*pData,cValid,&(adFadj[cIdxOff]));
 
-    for(i=cTrain; i < cTrain+cValid; i++)
-      {
-        adF[i + cIdxOff] += adFadj[i + cIdxOff];
-      }
+  for(i=cTrain; i < cTrain+cValid; i++)
+  {
+    adF[i + cIdxOff] += adFadj[i + cIdxOff];
+  }
     
-    dValidError =
-      pDist->Deviance(pData->y_ptr(),
-                      pData->misc_ptr(false),
-                      pData->offset_ptr(false),
-                      pData->weight_ptr(),
-                      adF,
-                      cValid,
-                      cIdxOff + cTrain);
+  dValidError =
+    pDist->Deviance(pData->y_ptr(),
+                    pData->misc_ptr(false),
+                    pData->offset_ptr(false),
+                    pData->weight_ptr(),
+                    adF,
+                    cValid,
+                    cIdxOff + cTrain);
 }
 
 
