0e58f8c6c0b754010310b0cbfbec1014f0ddf098
Tyler Hunt <tyler.hunt@utah.edu>
2015-10-12 16:58:37 -0600

    More specific test

--- tests/testthat/test_allmiss.R
+++ tests/testthat/test_allmiss.R
@@ -4,9 +4,11 @@ test_that("Columns with all missing stop", {
   
   iris2 <- iris
   iris2$Sepal.Width = NA
-  expect_error(gbm(Species == 'setosa' ~ ., data = iris2, distribution = 'bernoulli', n.trees = 2))
+  expect_error(gbm(Species == 'setosa' ~ ., data = iris2, distribution = 'bernoulli', n.trees = 2)
+               ,'variable\\(s\\) 2\\: Sepal.Width contain only missing values\\.')
 
   iris2$Petal.Width = NA
-  expect_error(gbm(Species == 'setosa' ~ ., data = iris2, distribution = 'bernoulli', n.trees = 2))
+  expect_error(gbm(Species == 'setosa' ~ ., data = iris2, distribution = 'bernoulli', n.trees = 2)
+               ,'variable\\(s\\) 2, 4\\: Sepal.Width, Petal.Width contain only missing values\\.')
   
   })
