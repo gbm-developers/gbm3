1ea633f8bc24e7ecb2f4b4850aa82510844a88fe
J HICKEY <james.hickey@cantab.net>
2016-07-20 17:33:02 +0100

    Removal of older functions

--- R/guessDist.R
+++
@@ -1,8 +0,0 @@
-guessDist <- function(y){
-    # If distribution is not given, try to guess it
-    if (length(unique(y)) == 2){ d <- "bernoulli" }
-    else if (class(y) == "Surv" ){ d <- "coxph" }
-    else{ d <- "gaussian" }
-    message(paste("Distribution not specified, assuming", d, "...\n"))
-    list(name=d)
-}
