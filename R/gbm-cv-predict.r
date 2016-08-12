e3991d1ae8bcbff4e142652fd049cc3e8b0d8932
Paul Metcalfe <Paul.Metcalfe@astrazeneca.com>
2016-08-12 09:22:45 +0100

    Correctly export predict as a method on stats::predict

--- R/gbm-cv-predict.r
+++ R/gbm-cv-predict.r
@@ -1,29 +1,30 @@
-# Predictions for CV fitted GBM models
-# 
-# Calculate predictions from cross validated generalized boosting model from gbm2.
-# 
-# @usage predict(gbm_cv_fit, gbm_data_obj, cv_folds, cv_group, best_iter_cv)
-# 
-# @param gbm_cv_fit a GBMCVFit object containing CV gbm models
-# 
-# @param gbm_data_obj a GBMData object containing all of the data used to fit a gbm model. 
-# 
-# @param cv_folds a positive integer specifying the number of folds to be used in cross-validation of the gbm
-# fit.
-# 
-# @param cv_group vector of integers specifying which row of data belongs to which cv_fold.
-# 
-# @param best_iter_cv number of trees with the smallest cv error
-# 
-# @author James Hickey
-#
-# @return a matrix of predictions for each cv fold.
-# 
-
-predict.GBMCVFit <- function(gbm_cv_fit, gbm_data_obj, cv_folds, cv_group, best_iter_cv) {
+#' Predictions for CV fitted GBM models
+#' 
+#' Calculate predictions from cross validated generalized boosting model from gbm2.
+#' 
+#' @param object a GBMCVFit object containing CV gbm models
+#' 
+#' @param gbm_data_obj a GBMData object containing all of the data used to fit a gbm model. 
+#' 
+#' @param cv_folds a positive integer specifying the number of folds to be used in cross-validation of the gbm
+#' fit.
+#' 
+#' @param cv_group vector of integers specifying which row of data belongs to which cv_fold.
+#' 
+#' @param best_iter_cv number of trees with the smallest cv error
+#'
+#' @param \dots not currently used
+#' 
+#' @author James Hickey
+#'
+#' @return a matrix of predictions for each cv fold.
+#' @importFrom stats predict
+#' @export
+predict.GBMCVFit <- function(object, gbm_data_obj, cv_folds,
+                             cv_group, best_iter_cv, ...) {
 
   # Extract data
-  data <- gbm_data_obj$original_data[seq_len(gbm_cv_fit[[1]]$params$num_train), ,drop=FALSE]
+  data <- gbm_data_obj$original_data[seq_len(object[[1]]$params$num_train), ,drop=FALSE]
   
   ## test cv_group and data match
   if (nrow(data) != length(cv_group)) {
@@ -37,7 +38,7 @@ predict.GBMCVFit <- function(gbm_cv_fit, gbm_data_obj, cv_folds, cv_group, best_
   
   for (ind in seq_len(cv_folds)) {
     flag <- cv_group == ind
-    model <- gbm_cv_fit[[ind+1]]
+    model <- object[[ind+1]]
     my_data  <- data[flag, model$variables$var_names, drop=FALSE]
     predictions <- predict(model, new_data=my_data, num_trees=best_iter_cv)
     predictions <- matrix(predictions, ncol=num_cols)
